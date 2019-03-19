a34768007a375ae04e8acddd79af72512c4e2169
Suraj Pathak <freesuraj@gmail.com>
2019-03-19 15:11:49 +1100

    Initial Commit

---
+++ QuickFire/QuickFire.h
@@ -0,0 +1,19 @@
+//
+//  QuickFire.h
+//  QuickFire
+//
+//  Created by Suraj Pathak on 19/3/19.
+//  Copyright © 2019 kakhara.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuickFire.
+FOUNDATION_EXPORT double QuickFireVersionNumber;
+
+//! Project version string for QuickFire.
+FOUNDATION_EXPORT const unsigned char QuickFireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickFire/PublicHeader.h>
+
+
